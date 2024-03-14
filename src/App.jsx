4a6c69ef3dd64c598e75675364829fd1e300911c
timabs <timur.abd.nyc@gmail.com>
2024-03-14 15:05:20 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header className="flex justify-center h-1/6 items-center">
+        <h1 className="text-purple-800 font-poppins text-5xl">Copycats</h1>
+      </header>
+    </>
+  );
+}
